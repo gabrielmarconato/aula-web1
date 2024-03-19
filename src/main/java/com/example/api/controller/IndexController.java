868d804565966540b3cdcc042f1cf3fb7364ecfd
GuilhermeMarques1 <g-marques@outlook.com>
2024-03-19 19:56:34 -0300

    returning multiple objects

--- src/main/java/com/example/api/controller/IndexController.java
+++ src/main/java/com/example/api/controller/IndexController.java
@@ -1,5 +1,8 @@
 package com.example.api.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,14 +10,42 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.api.model.Usuario;
+
 @RestController
 @RequestMapping
 public class IndexController {
 
   @GetMapping(value = "/", produces = "application/json")
-  public ResponseEntity init(@RequestParam(value = "nome", defaultValue = "none") String nome) {
-    System.out.println("O nosso parametro e: " + nome);
-    return new ResponseEntity("Olá usuário " + nome, HttpStatus.OK);
+  public ResponseEntity<Usuario> init() {
+    Usuario usuario = new Usuario();
+    usuario.setId(1l);
+    usuario.setLogin("asaasas@asa.com");
+    usuario.setNome("asdasd");
+    usuario.setSenha("123123");
+
+    return new ResponseEntity(usuario, HttpStatus.OK);
   }
 
+  @GetMapping(value = "/get2", produces = "application/json")
+  public ResponseEntity init2() {
+    Usuario usuario1 = new Usuario();
+    Usuario usuario2 = new Usuario();
+
+    usuario1.setId(1);
+    usuario1.setLogin("guilherme");
+    usuario1.setLogin("gui1");
+    usuario1.setSenha("123123");
+
+    usuario2.setId(2);
+    usuario2.setNome("gabriel");
+    usuario2.setLogin("gabriel02");
+    usuario2.setSenha("123");
+
+    List<Usuario> list = new ArrayList<Usuario>();
+    list.add(usuario1);
+    list.add(usuario2);
+
+    return new ResponseEntity(list, HttpStatus.OK);
+  }
 }
