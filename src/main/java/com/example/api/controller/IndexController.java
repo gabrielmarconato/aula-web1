1940895e38137e7cece2a1a132f69c4fb2753d0e
GuilhermeMarques1 <g-marques@outlook.com>
2024-03-19 20:31:06 -0300

    fetch all and fetch by id

--- src/main/java/com/example/api/controller/IndexController.java
+++ src/main/java/com/example/api/controller/IndexController.java
@@ -2,21 +2,28 @@ package com.example.api.controller;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.api.model.Usuario;
+import com.example.api.repository.UsuarioRepository;
 
 @RestController
 @RequestMapping
 public class IndexController {
 
-  @GetMapping(value = "/", produces = "application/json")
+  @Autowired
+  private UsuarioRepository usuarioRepository;
+
+  @GetMapping(value = "/aaa", produces = "application/json")
   public ResponseEntity<Usuario> init() {
     Usuario usuario = new Usuario();
     usuario.setId(1l);
@@ -47,5 +54,25 @@ public class IndexController {
     list.add(usuario2);
 
     return new ResponseEntity(list, HttpStatus.OK);
+  }
+
+  @GetMapping(value = "/usuario/{id}", produces = "application/json")
+  public ResponseEntity<Usuario> get(@PathVariable(value = "id") Long id) {
+    try {
+      Optional<Usuario> usuario = usuarioRepository.findById(id);
+  
+      return new ResponseEntity(usuario.get(), HttpStatus.OK);
+    } catch(Exception e) {
+      return new ResponseEntity("No such user", HttpStatus.NOT_FOUND);
+    }
+  }
+
+  @GetMapping(value = "/", produces = "application/json")
+  public ResponseEntity<List<Usuario>> getAll() {
+
+    List<Usuario> list = (List<Usuario>) usuarioRepository.findAll();
+
+    return new ResponseEntity(list, HttpStatus.OK);
+
   }
 }
