4567a7e7ae5f29a14f86a953d12183bcdf39f8fa
GuilhermeMarques1 <g-marques@outlook.com>
2024-03-19 22:05:07 -0300

    complete CRUD

--- src/main/java/com/example/api/controller/IndexController.java
+++ src/main/java/com/example/api/controller/IndexController.java
@@ -7,8 +7,12 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -80,5 +84,26 @@ public class IndexController {
   public ResponseEntity relatorio(@PathVariable(value = "id") Long id, @PathVariable(value = "dias") Long dias) {
     
     return new ResponseEntity("Relatorio do usuario: " + id + " para os  " + dias + " anteriores", HttpStatus.OK);
+  }
+
+  @PostMapping(value = "/", produces = "application/json")
+  public ResponseEntity<Usuario> cadastrar(@RequestBody Usuario usuario) {
+    Usuario usuariosalvo = usuarioRepository.save(usuario);
+
+    return new ResponseEntity(usuariosalvo, HttpStatus.OK);
+  }
+
+  @PutMapping(value = "/", produces = "application/json")
+  public ResponseEntity<Usuario> atualizar(@RequestBody Usuario usuario) {
+    Usuario usuariosalvo = usuarioRepository.save(usuario);
+
+    return new ResponseEntity(usuariosalvo, HttpStatus.OK);
+  }
+
+  @DeleteMapping(value = "/{id}", produces = "application/json")
+  public String deletar(@PathVariable("id") Long id) {
+    usuarioRepository.deleteById(id);
+
+    return "ok";
   }
 }
