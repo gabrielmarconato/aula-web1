4cadf4acd21256b049dd2c5b81fda8b625f743de
ghmarconato <ghmarconato@gmail.com>
2024-03-28 16:32:41 -0300

    Operacao Unica

--- src/main/java/com/example/api/model/OpUnica.java
+++ src/main/java/com/example/api/model/OpUnica.java
@@ -1,5 +1,23 @@
 package com.example.api.model;
+import java.time.LocalDateTime;
 
 public class OpUnica {
+    private float valor;
+    private LocalDateTime data;
 
+    public float getValor(){
+        return valor;
+    }
+
+    public LocalDateTime getData(){
+        return data;
+    }
+
+    public void setValor(float valor){
+        this.valor = valor;
+    }
+
+    public void setData(LocalDateTime data){
+        this.data = data;
+    }
 }
