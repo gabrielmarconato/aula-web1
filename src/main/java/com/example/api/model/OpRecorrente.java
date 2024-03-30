908513243633088252f890f49202c1f0447c563e
ghmarconato <ghmarconato@gmail.com>
2024-03-30 10:43:08 -0300

    OpRecorrente

--- src/main/java/com/example/api/model/OpRecorrente.java
+++ src/main/java/com/example/api/model/OpRecorrente.java
@@ -1,5 +1,33 @@
 package com.example.api.model;
 
+import java.time.LocalDateTime;
+
 public class OpRecorrente {
+    private float valorRec;
+    private LocalDateTime dataInicial;
+    private long intervalo; // Em dias
+
+    public float getValorRec(){
+        return valorRec;
+    }
+
+    public LocalDateTime getData(){
+        return dataInicial;
+    }
+    public long getIntervalo(){
+        return intervalo;
+    }
+
+    public void setValor(float valorRec){
+        this.valorRec = valorRec;
+    }
+
+    public void setData(LocalDateTime dataInicial){
+        this.dataInicial = dataInicial;
+    }
+
+    public void setIntervalo(long intervalo){
+        this.intervalo = intervalo;
+    }
 
 }
